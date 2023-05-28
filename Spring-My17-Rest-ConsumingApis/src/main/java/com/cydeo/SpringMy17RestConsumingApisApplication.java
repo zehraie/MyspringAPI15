29f829e3139e1077cd802d71e443704a3bf33f51
zehrairkicatal <zehraie197@gmail.com>
2023-05-28 20:27:08 +0100

    RestConsuming API

---
+++ Spring-My17-Rest-ConsumingApis/src/main/java/com/cydeo/SpringMy17RestConsumingApisApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMy17RestConsumingApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMy17RestConsumingApisApplication.class, args);
+	}
+
+}
